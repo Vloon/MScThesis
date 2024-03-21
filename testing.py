9ab2bf61c9933e4ae36ecbeb965355f7959d5962
Vloon <pleunscholten@hotmail.com>
2024-03-21 09:25:07 +0100

    remove old files

--- testing.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-myCsvRow = 'S1;REST1;1000;250;-820.58;59.52'
-myCsvRow = myCsvRow.split(';')
-with open('myCSV.csv','a',newline='') as fd:
-    writer = csv.writer(fd, delimiter=';')
-    writer.writerow(myCsvRow)
